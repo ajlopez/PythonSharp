cea9bd317e22ec7f3512b984c78fc2c1f2793f41
ajlopez <webmaster@ajlopez.com>
2012-11-03 11:21:47 -0300

    DefCommand with first test

---
+++ Src/PythonSharp.Tests/Commands/DefCommandTests.cs
@@ -0,0 +1,26 @@
+﻿namespace PythonSharp.Tests.Commands
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Commands;
+    using PythonSharp.Expressions;
+
+    [TestClass]
+    public class DefCommandTests
+    {
+        [TestMethod]
+        public void CreateSimpleDefCommand()
+        {
+            IList<string> argumentNames = new string[] { "a", "b" };
+            ICommand body = new SetCommand("c", new ConstantExpression(1));
+            DefCommand command = new DefCommand("foo", argumentNames, body);
+
+            Assert.AreEqual("foo", command.Name);
+            Assert.AreEqual(argumentNames, command.ArgumentNames);
+            Assert.AreEqual(body, command.Body);
+        }
+    }
+}
---
+++ Src/PythonSharp/Commands/DefCommand.cs
@@ -0,0 +1,32 @@
+﻿namespace PythonSharp.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DefCommand : ICommand
+    {
+        private string name;
+        private IList<string> argumentNames;
+        private ICommand body;
+
+        public DefCommand(string name, IList<string> argumentNames, ICommand body)
+        {
+            this.name = name;
+            this.argumentNames = argumentNames;
+            this.body = body;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IList<string> ArgumentNames { get { return this.argumentNames; } }
+
+        public ICommand Body { get { return this.body; } }
+
+        public void Execute(Machine machine, BindingEnvironment environment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
