f468d3c5e95d45778dce05e0695875b8853e3fe6
ajlopez <webmaster@ajlopez.com>
2012-11-03 11:40:49 -0300

    DefinedFunction with first simple test

---
+++ Src/PythonSharp.Tests/Language/DefinedFunctionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace PythonSharp.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Commands;
+    using PythonSharp.Expressions;
+    using PythonSharp.Language;
+
+    [TestClass]
+    public class DefinedFunctionTests
+    {
+        [TestMethod]
+        public void CreateSimpleDefinedFunction()
+        {
+            IList<string> argumentNames = new string[] { "a", "b" };
+            ICommand body = new SetCommand("c", new NameExpression("a"));
+            DefinedFunction func = new DefinedFunction(argumentNames, body);
+
+            Assert.AreEqual(argumentNames, func.ArgumentNames);
+            Assert.AreEqual(body, func.Body);
+        }
+    }
+}
--- Src/PythonSharp/Language/DefinedFunction.cs
+++ Src/PythonSharp/Language/DefinedFunction.cs
@@ -4,8 +4,37 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using PythonSharp.Commands;
 
-    public class DefinedFunction
+    public class DefinedFunction : IFunction
     {
+        private IList<string> argumentNames;
+        private ICommand body;
+
+        public DefinedFunction(IList<string> argumentNames, ICommand body)
+        {
+            this.argumentNames = argumentNames;
+            this.body = body;
+        }
+
+        public ICollection<string> ArgumentNames { get { return this.argumentNames; } }
+
+        public ICommand Body { get { return this.body; } }
+
+        public object Apply(IList<object> arguments)
+        {
+            BindingEnvironment environment = new BindingEnvironment();
+
+            if (this.argumentNames != null)
+                for (int k = 0; k < this.argumentNames.Count; k++)
+                    if (arguments != null && arguments.Count > k)
+                        environment.SetValue(this.argumentNames[k], arguments[k]);
+                    else
+                        environment.SetValue(this.argumentNames[k], null);
+
+            this.body.Execute(null, environment);
+
+            return null;
+        }
     }
 }
