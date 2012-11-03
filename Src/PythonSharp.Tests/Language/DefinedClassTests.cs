827067886ff49d0f027277a37ad2322c73c2f6c7
ajlopez <webmaster@ajlopez.com>
2012-11-03 10:19:34 -0300

    DefinedClass with first classes

---
+++ Src/PythonSharp.Tests/Language/DefinedClassTests.cs
@@ -0,0 +1,39 @@
+﻿namespace PythonSharp.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Language;
+
+    [TestClass]
+    public class DefinedClassTests
+    {
+        [TestMethod]
+        public void DefineClassWithName()
+        {
+            DefinedClass klass = new DefinedClass("Spam");
+
+            Assert.AreEqual("Spam", klass.Name);
+        }
+
+        [TestMethod]
+        public void SetGetMethod()
+        {
+            DefinedClass klass = new DefinedClass("Spam");
+            IMethod method = new NativeMethod(DummyMethod);
+
+            klass.SetMethod("foo", method);
+
+            var result = klass.GetMethod("foo");
+            Assert.IsNotNull(result);
+            Assert.AreEqual(method, result);
+        }
+
+        private object DummyMethod(object target, IList<object> arguments)
+        {
+            return null;
+        }
+    }
+}
---
+++ Src/PythonSharp/Language/DefinedClass.cs
@@ -0,0 +1,30 @@
+﻿namespace PythonSharp.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DefinedClass : IType
+    {
+        private string name;
+        private IDictionary<string, IMethod> methods = new Dictionary<string, IMethod>();
+
+        public DefinedClass(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public void SetMethod(string name, IMethod method)
+        {
+            this.methods[name] = method;
+        }
+
+        public IMethod GetMethod(string name)
+        {
+            return this.methods[name];
+        }
+    }
+}
