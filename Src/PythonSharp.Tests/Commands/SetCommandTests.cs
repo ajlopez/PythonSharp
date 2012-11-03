fde78b5899aa3482f076096527527562c7f15278
ajlopez <webmaster@ajlopez.com>
2012-11-03 10:36:41 -0300

    SetCommand with first test

---
+++ Src/PythonSharp.Tests/Commands/SetCommandTests.cs
@@ -0,0 +1,26 @@
+﻿namespace PythonSharp.Tests.Commands
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Expressions;
+    using PythonSharp.Commands;
+
+    [TestClass]
+    public class SetCommandTests
+    {
+        [TestMethod]
+        public void SetVariable()
+        {
+            IExpression expression = new ConstantExpression(1);
+            NameExpression variable = new NameExpression("spam");
+            BindingEnvironment environment = new BindingEnvironment();
+            ICommand command = new SetCommand(variable, expression);
+            command.Execute(null, environment);
+
+            Assert.AreEqual(1, environment.GetValue("spam"));
+        }
+    }
+}
---
+++ Src/PythonSharp/Commands/SetCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace PythonSharp.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using PythonSharp.Expressions;
+
+    public class SetCommand : ICommand
+    {
+        private NameExpression variable;
+        private IExpression expression;
+
+        public SetCommand(NameExpression variable, IExpression expression)
+        {
+            this.variable = variable;
+            this.expression = expression;
+        }
+
+        public void Execute(Machine machine, BindingEnvironment environment)
+        {
+            environment.SetValue(this.variable.Name, this.expression.Evaluate(environment));
+        }
+    }
+}
