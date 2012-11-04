45604c46c0d7b4a8ef443089910883b40f65e326
ajlopez <webmaster@ajlopez.com>
2012-11-04 19:50:32 -0300

    Named Argument Expression with tests

---
+++ Src/PythonSharp.Tests/Expressions/NamedArgumentExpressionTests.cs
@@ -0,0 +1,23 @@
+﻿namespace PythonSharp.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Expressions;
+
+    [TestClass]
+    public class NamedArgumentExpressionTests
+    {
+        [TestMethod]
+        public void CreateAndEvaluateNamedArgumentExpression()
+        {
+            NamedArgumentExpression expression = new NamedArgumentExpression("a", new ConstantExpression(1));
+
+            Assert.AreEqual("a", expression.Name);
+            Assert.IsNotNull(expression.Expression);
+            Assert.AreEqual(1, expression.Evaluate(null));
+        }
+    }
+}
---
+++ Src/PythonSharp/Expressions/NamedArgumentExpression.cs
@@ -0,0 +1,28 @@
+﻿namespace PythonSharp.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class NamedArgumentExpression : IExpression
+    {
+        private string name;
+        private IExpression expression;
+
+        public NamedArgumentExpression(string name, IExpression expression)
+        {
+            this.name = name;
+            this.expression = expression;
+        }
+
+        public string Name { get { return this.name; } }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public object Evaluate(BindingEnvironment environment)
+        {
+            return this.expression.Evaluate(environment);
+        }
+    }
+}
