35798361d462ae04935f229d4b4d3a0cbcb60baa
ajlopez <webmaster@ajlopez.com>
2012-11-07 06:38:18 -0300

    Set Attribute with test

---
+++ Src/PythonSharp.Tests/Commands/SetAttributeCommandTests.cs
@@ -0,0 +1,31 @@
+﻿namespace PythonSharp.Tests.Commands
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Language;
+    using PythonSharp.Commands;
+    using PythonSharp.Expressions;
+
+    [TestClass]
+    public class SetAttributeCommandTests
+    {
+        [TestMethod]
+        public void SetAttributeInDynamicObject()
+        {
+            BindingEnvironment environment = new BindingEnvironment();
+            DefinedClass klass = new DefinedClass("Spam");
+            DynamicObject dynobj = new DynamicObject(klass);
+            environment.SetValue("foo", dynobj);
+
+            SetAttributeCommand command = new SetAttributeCommand(new NameExpression("foo"), "one", new ConstantExpression(1));
+
+            command.Execute(environment);
+
+            Assert.IsTrue(dynobj.HasValue("one"));
+            Assert.AreEqual(1, dynobj.GetValue("one"));
+        }
+    }
+}
---
+++ Src/PythonSharp/Commands/SetAttributeCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace PythonSharp.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using PythonSharp.Expressions;
+    using PythonSharp.Language;
+
+    public class SetAttributeCommand : ICommand
+    {
+        private IExpression targetExpression;
+        private string name;
+        private IExpression expression;
+
+        public SetAttributeCommand(IExpression targetExpression, string name, IExpression expression)
+        {
+            this.targetExpression = targetExpression;
+            this.name = name;
+            this.expression = expression;
+        }
+
+        public IExpression TargetExpression { get { return this.targetExpression; } }
+
+        public string Name { get { return this.name; } }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public void Execute(BindingEnvironment environment)
+        {
+            IValues values = (IValues) this.targetExpression.Evaluate(environment);
+            values.SetValue(this.name, this.expression.Evaluate(environment));
+        }
+    }
+}
