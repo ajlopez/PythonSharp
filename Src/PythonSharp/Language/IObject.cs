db619f8a47716447ed1bee367145a023febd4d67
ajlopez <webmaster@ajlopez.com>
2012-11-10 11:21:14 -0300

    ObjectUtilities with tests

---
+++ Src/PythonSharp/Language/IObject.cs
@@ -0,0 +1,12 @@
+﻿namespace PythonSharp.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public interface IObject : IValues
+    {
+        object Invoke(string name, IList<object> arguments);
+    }
+}
