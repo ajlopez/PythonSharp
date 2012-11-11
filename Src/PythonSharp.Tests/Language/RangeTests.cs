9a684aa1255e38f06c6af44090def40ae1436378
ajlopez <webmaster@ajlopez.com>
2012-11-11 11:51:38 -0300

    Range with tests

---
+++ Src/PythonSharp.Tests/Language/RangeTests.cs
@@ -0,0 +1,88 @@
+﻿namespace PythonSharp.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Language;
+    using System.Collections;
+
+    [TestClass]
+    public class RangeTests
+    {
+        [TestMethod]
+        public void CreateSimpleRange()
+        {
+            Range range = new Range(2);
+
+            var list = range.ToList();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(0, list[0]);
+            Assert.AreEqual(1, list[1]);
+        }
+
+        [TestMethod]
+        public void CreateRangeFromTo()
+        {
+            Range range = new Range(2, 4);
+
+            var list = range.ToList();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(2, list[0]);
+            Assert.AreEqual(3, list[1]);
+        }
+
+        [TestMethod]
+        public void CreateRangeFromToStep()
+        {
+            Range range = new Range(2, 6, 2);
+
+            var list = range.ToList();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(2, list[0]);
+            Assert.AreEqual(4, list[1]);
+        }
+
+        [TestMethod]
+        public void CreateRangeFromToNegativeStep()
+        {
+            Range range = new Range(6, 2, -2);
+
+            var list = range.ToList();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(2, list.Count);
+            Assert.AreEqual(6, list[0]);
+            Assert.AreEqual(4, list[1]);
+        }
+
+        [TestMethod]
+        public void CreateEmptyRangeFromTo()
+        {
+            Range range = new Range(2, -4);
+
+            var list = range.ToList();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod]
+        public void CreateSimpleEmptyRange()
+        {
+            Range range = new Range(0);
+
+            var list = range.ToList();
+
+            Assert.IsNotNull(list);
+            Assert.AreEqual(0, list.Count);
+        }
+    }
+}
---
+++ Src/PythonSharp/Language/Range.cs
@@ -0,0 +1,52 @@
+﻿namespace PythonSharp.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Collections;
+
+    public class Range : IEnumerable
+    {
+        private int from;
+        private int to;
+        private int step;
+
+        public Range(int to)
+            : this(0, to, 1)
+        {
+        }
+
+        public Range(int from, int to)
+            : this(from, to, 1)
+        {
+        }
+
+        public Range(int from, int to, int step)
+        {
+            this.from = from;
+            this.to = to;
+            this.step = step;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            if (this.step < 0)
+                for (int k = this.from; k > this.to; k += this.step)
+                    yield return k;
+            else
+                for (int k = this.from; k < this.to; k += this.step)
+                    yield return k;
+        }
+
+        public IList ToList()
+        {
+            var list = new List<object>();
+
+            foreach (var item in this)
+                list.Add(item);
+
+            return list;
+        }
+    }
+}
