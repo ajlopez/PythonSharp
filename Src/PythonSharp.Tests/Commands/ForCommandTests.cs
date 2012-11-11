187c4b2664e341e9736632759e4a5a8289041062
ajlopez <webmaster@ajlopez.com>
2012-11-11 11:14:03 -0300

    ForCommand with first tests

---
+++ Src/PythonSharp.Tests/Commands/ForCommandTests.cs
@@ -0,0 +1,84 @@
+﻿namespace PythonSharp.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Commands;
+    using PythonSharp.Expressions;
+    using PythonSharp.Language;
+    using PythonSharp.Exceptions;
+
+    [TestClass]
+    public class ForCommandTests
+    {
+        [TestMethod]
+        public void ExecuteSimpleFor()
+        {
+            ICommand body = new SetCommand("b", new BinaryOperatorExpression(new NameExpression("a"), new NameExpression("b"), BinaryOperator.Add));
+            BindingEnvironment environment = new BindingEnvironment();
+            environment.SetValue("b", 0);
+
+            ForCommand command = new ForCommand("a", new ConstantExpression(new object[] { 1, 2, 3 }), body);
+
+            command.Execute(environment);
+
+            Assert.AreEqual(6, environment.GetValue("b"));
+        }
+
+        [TestMethod]
+        public void ExecuteSimpleForOnEmptyList()
+        {
+            ICommand body = new SetCommand("b", new BinaryOperatorExpression(new NameExpression("a"), new NameExpression("b"), BinaryOperator.Add));
+            BindingEnvironment environment = new BindingEnvironment();
+            environment.SetValue("b", 0);
+
+            ForCommand command = new ForCommand("a", new ConstantExpression(new object[] { }), body);
+
+            command.Execute(environment);
+
+            Assert.AreEqual(0, environment.GetValue("b"));
+        }
+
+        [TestMethod]
+        public void RaiseWhenForOverNone()
+        {
+            BindingEnvironment environment = new BindingEnvironment();
+            environment.SetValue("b", 0);
+
+            ForCommand command = new ForCommand("a", new ConstantExpression(null), null);
+
+            try
+            {
+                command.Execute(environment);
+                Assert.Fail("Exception expected");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(TypeError));
+                Assert.AreEqual("'NoneType' object is not iterable", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void RaiseWhenForOverAnInteger()
+        {
+            BindingEnvironment environment = new BindingEnvironment();
+            environment.SetValue("b", 0);
+
+            ForCommand command = new ForCommand("a", new ConstantExpression(1), null);
+
+            try
+            {
+                command.Execute(environment);
+                Assert.Fail("Exception expected");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(TypeError));
+                Assert.AreEqual("'int' object is not iterable", ex.Message);
+            }
+        }
+    }
+}
---
+++ Src/PythonSharp/Commands/ForCommand.cs
@@ -0,0 +1,47 @@
+﻿namespace PythonSharp.Commands
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    using PythonSharp.Expressions;
+    using PythonSharp.Language;
+    using System.Collections;
+    using PythonSharp.Exceptions;
+
+    public class ForCommand : ICommand
+    {
+        private string name;
+        private IExpression expression;
+        private ICommand command;
+
+        public ForCommand(string name, IExpression expression, ICommand command)
+        {
+            this.name = name;
+            this.expression = expression;
+            this.command = command;
+        }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public ICommand Command { get { return this.command; } }
+
+        public void Execute(IContext context)
+        {
+            var environment = context as BindingEnvironment;
+            var value = this.expression.Evaluate(context);
+            var items = value as IEnumerable;
+
+            if (items == null)
+                throw new TypeError(string.Format("'{0}' object is not iterable", Types.GetTypeName(value)));
+
+            foreach (var item in items)
+            {
+                context.SetValue(this.name, item);
+                this.command.Execute(context);
+                if (environment != null && environment.HasReturnValue())
+                    return;
+            }
+        }
+    }
+}
