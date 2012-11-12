d6c2ed51dedc0b9d9cb99cef0f288f03d1d0d13a
ajlopez <webmaster@ajlopez.com>
2012-11-12 06:57:07 -0300

    EvalFunction with first tests

---
+++ Src/PythonSharp.Tests/Functions/EvalFunctionTests.cs
@@ -0,0 +1,43 @@
+﻿namespace PythonSharp.Tests.Functions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Functions;
+    using PythonSharp.Exceptions;
+
+    [TestClass]
+    public class EvalFunctionTests
+    {
+        private EvalFunction eval;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this.eval = new EvalFunction();
+        }
+
+        [TestMethod]
+        public void EvalSimpleText()
+        {
+            Assert.AreEqual(0, eval.Apply(null, new object[] { "0" }, null));
+        }
+
+        [TestMethod]
+        public void RaiseIfNoArgument()
+        {
+            try
+            {
+                eval.Apply(null, null, null);
+                Assert.Fail("Exception expected");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(TypeError));
+                Assert.AreEqual("eval expected at least 1 arguments, got 0", ex.Message);
+            }
+        }
+    }
+}
---
+++ Src/PythonSharp/Functions/EvalFunction.cs
@@ -0,0 +1,35 @@
+﻿namespace PythonSharp.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using PythonSharp.Exceptions;
+    using PythonSharp.Language;
+    using PythonSharp.Compiler;
+    using PythonSharp.Expressions;
+
+    public class EvalFunction : IFunction
+    {
+        public object Apply(IContext context, IList<object> arguments, IDictionary<string, object> namedArguments)
+        {
+            int nargs = arguments == null ? 0 : arguments.Count;
+
+            if (nargs == 0)
+                throw new TypeError("eval expected at least 1 arguments, got 0");
+
+            // TODO implement bytes or code object
+            if (!(arguments[0] is string))
+                throw new TypeError("eval() arg 1 must be a string, bytes or code object");
+
+            Parser parser = new Parser((string)arguments[0]);
+
+            IExpression expression = parser.CompileExpression();
+
+            if (expression == null)
+                return null;
+
+            return expression.Evaluate(context);
+        }
+    }
+}
