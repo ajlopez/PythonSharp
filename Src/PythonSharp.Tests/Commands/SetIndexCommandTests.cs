506b3a222226e9ae1fdbe2a7d7379c898c5e35d5
ajlopez <webmaster@ajlopez.com>
2012-11-13 07:21:57 -0300

    Set Index Command with tests

---
+++ Src/PythonSharp.Tests/Commands/SetIndexCommandTests.cs
@@ -0,0 +1,71 @@
+﻿namespace PythonSharp.Tests.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using PythonSharp.Commands;
+    using PythonSharp.Expressions;
+    using System.Collections;
+
+    [TestClass]
+    public class SetIndexCommandTests
+    {
+        [TestMethod]
+        public void CreateSetIndexCommand()
+        {
+            IExpression targetExpression = new ConstantExpression(1);
+            IExpression indexExpression = new ConstantExpression(2);
+            IExpression valueExpression = new ConstantExpression(3);
+
+            var command = new SetIndexCommand(targetExpression, indexExpression, valueExpression);
+
+            Assert.AreEqual(targetExpression, command.TargetExpression);
+            Assert.AreEqual(indexExpression, command.IndexExpression);
+            Assert.AreEqual(valueExpression, command.Expression);
+        }
+
+        [TestMethod]
+        public void ExecuteSetIndexCommandOnArray()
+        {
+            var array = new object[] { 1, 2, 2 };
+            IExpression targetExpression = new ConstantExpression(array);
+            IExpression indexExpression = new ConstantExpression(2);
+            IExpression valueExpression = new ConstantExpression(3);
+
+            var command = new SetIndexCommand(targetExpression, indexExpression, valueExpression);
+            command.Execute(null);
+
+            Assert.AreEqual(3, array[2]);
+        }
+
+        [TestMethod]
+        public void ExecuteSetIndexCommandOnList()
+        {
+            var list = new List<object>() { 1, 2, 2 };
+            IExpression targetExpression = new ConstantExpression(list);
+            IExpression indexExpression = new ConstantExpression(2);
+            IExpression valueExpression = new ConstantExpression(3);
+
+            var command = new SetIndexCommand(targetExpression, indexExpression, valueExpression);
+            command.Execute(null);
+
+            Assert.AreEqual(3, list[2]);
+        }
+
+        [TestMethod]
+        public void ExecuteSetIndexCommandOnDictionary()
+        {
+            var dictionary = new Hashtable();
+            IExpression targetExpression = new ConstantExpression(dictionary);
+            IExpression indexExpression = new ConstantExpression("foo");
+            IExpression valueExpression = new ConstantExpression("bar");
+
+            var command = new SetIndexCommand(targetExpression, indexExpression, valueExpression);
+            command.Execute(null);
+
+            Assert.AreEqual("bar", dictionary["foo"]);
+        }
+    }
+}
---
+++ Src/PythonSharp/Commands/SetIndexCommand.cs
@@ -0,0 +1,39 @@
+﻿namespace PythonSharp.Commands
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using PythonSharp.Expressions;
+    using PythonSharp.Language;
+    using PythonSharp.Utilities;
+
+    public class SetIndexCommand : ICommand
+    {
+        private IExpression targetExpression;
+        private IExpression indexExpression;
+        private IExpression expression;
+
+        public SetIndexCommand(IExpression targetExpression, IExpression indexExpression, IExpression expression)
+        {
+            this.targetExpression = targetExpression;
+            this.indexExpression = indexExpression;
+            this.expression = expression;
+        }
+
+        public IExpression TargetExpression { get { return this.targetExpression; } }
+
+        public IExpression IndexExpression { get { return this.indexExpression; } }
+
+        public IExpression Expression { get { return this.expression; } }
+
+        public void Execute(IContext context)
+        {
+            var target = this.targetExpression.Evaluate(context);
+            var index = this.indexExpression.Evaluate(context);
+            var value = this.expression.Evaluate(context);
+
+            ObjectUtilities.SetIndexedValue(target, new object[] { index }, value);
+        }
+    }
+}
