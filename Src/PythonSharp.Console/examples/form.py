07a13daf731bfbef57d4f055fa132de9d47fbad6
ajlopez <webmaster@ajlopez.com>
2012-11-16 18:08:05 -0300

    form.py example

---
+++ Src/PythonSharp.Console/examples/form.py
@@ -0,0 +1,19 @@
+﻿
+from System.Windows.Forms import Form, Button, Application
+from PythonSharp.Utilities import ObjectUtilities
+
+f = Form()
+f.Text = "PythonSharp Form"
+b = Button()
+b.Text = "Hello"
+b.Width = 150
+b.Height = 50
+f.Controls.Add(b)
+
+def click(sender, event):
+   print("Click")
+
+ObjectUtilities.AddHandler(b, "Click", click, locals())
+
+Application.Run(f)
+
